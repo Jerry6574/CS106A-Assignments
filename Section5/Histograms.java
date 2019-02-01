6adeec08624c85e24329d8658faf336ae0f790f1
Jerry <271222434@qq.com>
2019-02-02 07:23:05 +0800

    add Histograms.java to section5 assignment

---
+++ Section5/Histograms.java
@@ -0,0 +1,62 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+import acm.program.*;
+import acm.util.ErrorException;
+
+public class Histograms extends ConsoleProgram{
+	public void run() {
+    	String filename = readLine("MidTermScore File: ");
+    	BufferedReader rd = openFile(filename);
+    	while(rd == null) {
+    		filename = readLine("File: ");
+    		rd = openFile(filename);
+    	}
+    	
+    	String[] midTermScoreLabels = {"00-09: ", "10-19: ", "20-29: ", "30-39: ", "40-49: ", 
+    								   "50-59: ", "60-69: ", "70-79: ", "80-89: ", "90-99: ", "  100: "};
+    	String[] scoreCounts = new String [11];
+    	for(int i = 0; i < scoreCounts.length; i++) {
+    		scoreCounts[i] = "";
+    	}
+    	
+    	int[] midTermScoreBins = new int[11];
+    	
+		try {
+			while(true){
+				String line = rd.readLine();
+				if(line == null) {
+					break;
+				}
+				int score = Integer.parseInt(line);
+				
+				int binIndex = score / 10;
+				midTermScoreBins[binIndex] += 1;
+			}
+			rd.close();
+			
+			for(int i = 0; i < midTermScoreBins.length; i++) {
+				for(int j = 0; j < midTermScoreBins[i]; j++) {
+					scoreCounts[i] += "*";
+				}
+				println(midTermScoreLabels[i] + scoreCounts[i]);
+			}
+		} 
+		catch(IOException ex){
+			throw new ErrorException(ex);
+		}
+	}
+	
+    private BufferedReader openFile(String filename){
+    	BufferedReader rd = null;
+    	
+		try{
+			rd = new BufferedReader(new FileReader(filename));
+		} catch(IOException ex){
+			println("Nice try punk. That file doesn't exist. ");
+		}
+		
+    	return rd;
+    }
+}
--- Section5/WordCount.java
+++ Section5/WordCount.java
@@ -1,8 +1,6 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.io.PrintWriter;
 
 import acm.program.*;
 import acm.util.ErrorException;
@@ -11,6 +9,10 @@ public class WordCount extends ConsoleProgram {
     public void run() {
     	String filename = readLine("File: ");
     	BufferedReader rd = openFile(filename);
+    	while(rd == null) {
+    		filename = readLine("File: ");
+    		rd = openFile(filename);
+    	}
     	
     	int lineCount = 0;
     	int charCount = 0; 
@@ -29,10 +31,12 @@ public class WordCount extends ConsoleProgram {
 					prevCharIsLetterOrDigit = Character.isLetterOrDigit(line.charAt(i));
 					curCharIsLetterOrDigit = Character.isLetterOrDigit(line.charAt(i+1));
 					
-					// word counting mechanism
+					// word counting mechanism. 
+					// increment word count when change from LetterOrDigit to non-LetterOrDigit:
+					// -> && prevCharIsLetterOrDigit to avoid duplicated counting.
+					// -> count words at end of line that have no punctuation. 
 					if((prevCharIsLetterOrDigit != curCharIsLetterOrDigit && prevCharIsLetterOrDigit) ||
 					   (i == line.length()-2 && curCharIsLetterOrDigit)) {
-						println(line.charAt(i) + " + " + line.charAt(i+1));
 						wordCount++;
 					}
 				}
@@ -51,13 +55,13 @@ public class WordCount extends ConsoleProgram {
     private BufferedReader openFile(String filename){
     	BufferedReader rd = null;
     	
-    	while(rd == null){
-    		try{
-    			rd = new BufferedReader(new FileReader(filename));
-    		} catch(IOException ex){
-    			println("Nice try punk. That file doesn't exist. ");
-    		}
-    	}
+		try{
+			rd = new BufferedReader(new FileReader(filename));
+		} catch(IOException ex){
+			println("Nice try punk. That file doesn't exist. ");
+		}
+		
     	return rd;
     }
 }
+
