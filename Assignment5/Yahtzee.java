3744f3505dcc61c288ebc2e681f146b7d4569a1d
Jerry <271222434@qq.com>
2019-03-31 22:05:22 +0800

    added generate random dice

--- Assignment5/Yahtzee.java
+++ Assignment5/Yahtzee.java
@@ -17,18 +17,28 @@ public class Yahtzee extends GraphicsProgram implements YahtzeeConstants {
 	}
 	
 	public void run() {
-		IODialog dialog = getDialog();
-		nPlayers = dialog.readInt("Enter number of players");
-		playerNames = new String[nPlayers];
-		for (int i = 1; i <= nPlayers; i++) {
-			playerNames[i - 1] = dialog.readLine("Enter name for player " + i);
-		}
+//		IODialog dialog = getDialog();
+//		nPlayers = dialog.readInt("Enter number of players");
+//		playerNames = new String[nPlayers];
+//		for (int i = 1; i <= nPlayers; i++) {
+//			playerNames[i - 1] = dialog.readLine("Enter name for player " + i);
+//		}
+		
+		// Temperary playerNames
+		String[] playerNames = {"Jerry", "Qiwen"};
+		
 		display = new YahtzeeDisplay(getGCanvas(), playerNames);
 		playGame();
 	}
 
 	private void playGame() {
-		int[] dice = {1, 2, 3, 4, 6};
+		
+		// generate random dice
+		int[] dice = new int[N_DICE];
+		for(int i = 0; i < N_DICE; i++) {
+			dice[i] = rgen.nextInt(1, 6);
+		}
+		
 		display.displayDice(dice);
 	}
 		
